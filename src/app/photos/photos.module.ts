59e40339e312d396d29e26836636008f24a57735
goethecarvalho <goethecarvalho@gmail.com>
2020-05-17 12:04:05 -0300

    Alura - Angular - Módulo 01 - Fundamentos

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -1,26 +1,13 @@
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';
-
-import { PhotoListComponent } from './photo-list/photo-list.component';
-import { PhotoComponent } from './photo/photo.component';
-import { PhotoFormComponent } from './photo-form/photo-form.component';
-import { PhotosComponent } from './photo-list/photos/photos.component';
-import { FilterByDescription } from './photo-list/filter-by-description.pipe';
-import { LoadButtonComponent } from './photo-list/load-button/load-button.component';
+import { PhotoModule } from './photo/photo.module';
+import { PhotoFormModule } from './photo-form/photo-form.module';
+import { PhotoListModule } from './photo-list/photo-list.module';
 
 @NgModule({
-    declarations:[
-        PhotoComponent, 
-        PhotoListComponent, 
-        PhotoFormComponent, 
-        PhotosComponent,
-        FilterByDescription,
-        LoadButtonComponent
-    ],
-    imports:[ 
-        HttpClientModule,
-        CommonModule
+    imports:[
+        PhotoModule,
+        PhotoFormModule,
+        PhotoListModule
     ]
 })
 export class PhotosModule{}
