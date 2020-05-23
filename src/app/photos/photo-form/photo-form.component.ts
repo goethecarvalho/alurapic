4c37306bada7d59a6f5bca648cc720175b66bb13
goethecarvalho <goethecarvalho@gmail.com>
2020-05-23 10:34:02 -0300

    Angular parte 03 - Preview de imagens

--- src/app/photos/photo-form/photo-form.component.ts
+++ src/app/photos/photo-form/photo-form.component.ts
@@ -12,6 +12,7 @@ export class PhotoFormComponent implements OnInit {
 
   photoForm: FormGroup;
   file: File;
+  preview: string;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -35,5 +36,11 @@ export class PhotoFormComponent implements OnInit {
       .upload(description, allowComments, this.file)
       .subscribe(() => this.router.navigate(['']))
 
+  }
+  handleFile(file: File) {
+    this.file = file;
+    const reader = new FileReader();
+    reader.onload = (event: any) => this.preview = event.target.result;
+    reader.readAsDataURL(file);
   }
 }
